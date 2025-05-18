a66081345555947ad0242dc5288cd0f0fc1dd1c1
deltron9 <you@example.com>
2025-05-18 10:58:12 -0300

    Actualizacion de código de clase Libro

--- Libro/src/libro/Libro.java
+++ Libro/src/libro/Libro.java
@@ -32,10 +32,27 @@ public class Libro {
         return numeroDePaginas;
     }
 
+    public int getPaginaActual() {
+        return this.paginaActual;
+    }
+
     public void avanzarPagina(int paginas) {
         if (paginas > numeroDePaginas) {
             throw new IllegalArgumentException("Sobrepasas la cantidad de páginas del libro.");
+        } else if (paginaActual == numeroDePaginas) {
+            informarLibroTerminado();
         }
         this.paginaActual += paginas;
+    }
+
+    public void retrocederPagina(int paginas) {
+        if (paginas <= 1) {
+            throw new IllegalArgumentException("numero de pagina inválido.");
+        }
+        this.paginaActual -= paginas;
+    }
+
+    public String informarLibroTerminado() {
+        return "Libro terminado.";
     }
 }
