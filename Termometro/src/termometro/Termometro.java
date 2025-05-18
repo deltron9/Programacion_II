c622cafd58783dfbaaa2bd9150a0da0d1f93b1c3
deltron9 <you@example.com>
2025-05-18 11:48:26 -0300

    Actualización de código

--- Termometro/src/termometro/Termometro.java
+++ Termometro/src/termometro/Termometro.java
@@ -1,20 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package termometro;
 
 /**
- *
- * @author deltron97
+ * 7. Clase Termómetro: o Crea una clase Termómetro con un atributo temperatura
+ * de tipo double. o Escribe métodos para convertir la temperatura de grados
+ * Celsius a Fahrenheit y viceversa. o Implementa un método que permita aumentar
+ * o disminuir la temperatura en un valor dado.
  */
 public class Termometro {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private double temperatura;
+
+    public Termometro(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public double convertirEnFahrenheit(double temperatura) {
+        return this.temperatura = (temperatura * 9 / 5) + 32;
+    }
+
+    public double convertirEnCelcius(double temperatura) {
+        return this.temperatura = (temperatura - 32) * 5 / 9;
+    }
+
+    public double aumentarTemperatura(double temperatura) {
+        if (temperatura > 0) {
+            return this.temperatura + temperatura;
+        } else if (temperatura < 0) {
+            throw new IllegalArgumentException("Tenés que pasar un valor positivo para poder incrementar la temperatura");
+        }
+        return temperatura;
     }
     
+    public double disminuirTemperatura(double temperatura) {
+        if (temperatura < 0) {
+            return this.temperatura - temperatura;
+        } else if (temperatura > 0) {
+            throw new IllegalArgumentException("Tenés que pasar un valor positivo para poder disminuir la temperatura");
+        }
+        return temperatura;
+    }
+
 }
